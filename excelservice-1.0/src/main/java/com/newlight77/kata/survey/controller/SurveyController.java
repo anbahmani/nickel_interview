d9bbf75adcc64c37284a53ca83d17d9b420c6779
Anis <anisbahmani@hotmail.fr>
2024-05-14 14:14:23 +0200

    use PostMapping and GetMapping instead of RequestMapping

--- excelservice-1.0/src/main/java/com/newlight77/kata/survey/controller/SurveyController.java
+++ excelservice-1.0/src/main/java/com/newlight77/kata/survey/controller/SurveyController.java
@@ -14,27 +14,27 @@ public class SurveyController {
       this.exportCampaignService = exportCampaignService;
     }
 
-    @RequestMapping(value = "/api/survey/create", method = RequestMethod.POST)
+    @PostMapping("/api/survey/create")
     public void createSurvey(@RequestBody final Survey survey) {
         exportCampaignService.createSurvey(survey);
     }
 
-    @RequestMapping(value = "/api/survey/get", method = RequestMethod.GET)
+    @GetMapping("/api/survey/get")
     public Survey getSurvey(@RequestParam final String id) {
         return exportCampaignService.getSurvey(id);
     }
 
-    @RequestMapping(value = "/api/survey/campaign/create", method = RequestMethod.POST)
+    @PostMapping("/api/survey/campaign/create")
     public void createCampaign(@RequestBody final Campaign campaign) {
         exportCampaignService.createCampaign(campaign);
     }
 
-    @RequestMapping(value = "/api/survey/campaign/get", method = RequestMethod.GET)
+    @GetMapping("/api/survey/campaign/get")
     public Campaign getCampaign(@RequestParam final String id) {
         return exportCampaignService.getCampaign(id);
     }
 
-    @RequestMapping(value = "/api/survey/campaign/export", method = RequestMethod.POST)
+    @PostMapping("/api/survey/campaign/export")
     public void exportCampaign(@RequestParam final String campaignId) {
 
         final Campaign campaign = exportCampaignService.getCampaign(campaignId);
